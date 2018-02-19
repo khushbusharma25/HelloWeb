5803f5f190db334deed3c7d04254ea81f23c336c
khushbusharma25 <34641339+khushbusharma25@users.noreply.github.com>
2018-02-19 12:42:50 +0530

    dto and test

---
+++ Hello/src/main/java/com/js/dto/StudentDTO.java
@@ -0,0 +1,19 @@
+package com.js.dto;
+
+public class StudentDTO {
+	 private int id;
+	 private int rno;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getRno() {
+		return rno;
+	}
+	public void setRno(int rno) {
+		this.rno = rno;
+	}
+	 
+}
---
+++ Hello/src/test/java/test.java
@@ -0,0 +1,7 @@
+import com.js.dto.StudentDTO;
+
+public class test {
+ public static void main(String[] args) {
+	System.out.println(new StudentDTO());
+}
+}
